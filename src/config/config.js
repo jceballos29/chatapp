27236941b28d3c6a784e776ecf07a143e6ce19f1
Juan Ceballos <jacu29@gmail.com>
2021-09-28 21:05:50 -0500

    Cambiadno la cadena de conexion de prod

--- src/config/config.js
+++ src/config/config.js
@@ -16,10 +16,18 @@ module.exports = {
     dialect: "postgres"
   },
   production: {
-    username: "root",
-    password: null,
-    database: "database_production",
-    host: "127.0.0.1",
-    dialect: "mysql"
+    // username: process.env.DB_USERNAME,
+    // password: process.env.DB_PASSWORD,
+    // database: process.env.DB_NAME,
+    // host: process.env.DB_HOST,
+    // dialect: "postgres"
+
+    user_env_variable: "DATABASE_URL",
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false
+      }
+    }
   }
 }
