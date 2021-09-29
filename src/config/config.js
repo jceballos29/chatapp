db820e467efa3f265bbfc0c9333ace136d2d0a7a
Juan Ceballos <jacu29@gmail.com>
2021-09-28 21:09:09 -0500

    Cambiadno la cadena de conexion de prod

--- src/config/config.js
+++ src/config/config.js
@@ -21,13 +21,12 @@ module.exports = {
     // database: process.env.DB_NAME,
     // host: process.env.DB_HOST,
     // dialect: "postgres"
-
-    user_env_variable: "DATABASE_URL",
+    use_env_variable: 'DATABASE_URL',
     dialectOptions: {
-      ssl: {
-        require: true,
-        rejectUnauthorized: false
-      }
+        ssl: {
+            require: true,
+            rejectUnauthorized: false
+        }
     }
   }
 }
